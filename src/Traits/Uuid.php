f9e8446695adb7d8718e4ccac54000c3697cc776
Caio Felipe <caio@devapps.com.br>
2026-06-03 09:41:14 -0300

    feat: use UUID on creating trait

---
+++ src/Traits/Uuid.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace DevApps\LaravelModulesKit\Traits;
+
+use Illuminate\Support\Str;
+
+trait Uuid
+{
+    protected static function bootUuid()
+    {
+        static::creating(function ($model) {
+            if (empty($model->uuid)) {
+                $model->uuid = Str::uuid()->toString();
+            }
+        });
+    }
+
+    protected function getRouteKeyName()
+    {
+        return 'uuid';
+    }
+}
